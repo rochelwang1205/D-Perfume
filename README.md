4d85d564612fa0af52639b6788ba3b7036f0a75c
Rochel <liuyingfeng57@gmail.com>
2024-07-14 11:02:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# D'Perfume
+## Description
+- 2024六角學院切版專題班個人專題申請考核項目
+- 首頁(Home)及產品頁(product1)
+
+## 資料夾結構
+  - assets # 靜態資源放置處
+    - images # 圖片放置處
+    - scss # SCSS 的樣式放置處
+  - layout # ejs 模板放置處
+  - pages # 頁面放置處
+  - main.js 
+
+## 里程碑
+- 24/07/13, 24/07/14 home sections including RWD
+- 24/07/12 nav(RWD) cart icon + product1 including RWD + CSS revised (3.5hrs)
+- 24/07/11 subscription including RWD (1hr) + home including RWD (3.5hrs)
+- 24/07/10 environment set up ,navbar & footer including RWD (4hrs)
+
+## reference
+-[CSS 系列 -- 块级元素靠右的实现方式]('https://juejin.cn/post/6985480302030225415')
+-[flex-direction 屬性介紹]('https://w3c.hexschool.com/flexbox/c1b62b9b')
+-[[CSS筆記]CSS如何讓圖片呈現正方形等比例縮放]('https://medium.com/@martina.says/css%E7%AD%86%E8%A8%98-css%E5%A6%82%E4%BD%95%E8%AE%93%E5%9C%96%E7%89%87%E5%91%88%E7%8F%BE%E6%AD%A3%E6%96%B9%E5%BD%A2%E7%AD%89%E6%AF%94%E4%BE%8B%E7%B8%AE%E6%94%BE-107148b4f177')
+
+### Author
+- Website - [Rochel Wang](https://github.com/rochelwang1205)
+- Frontend Mentor - [@Rochel Wang](https://www.frontendmentor.io/profile/rochelwang1205)
+- Twitter - [@RochelWang4](https://twitter.com/RochelWang4)
---
+++ main.js
@@ -0,0 +1,38 @@
+import './assets/scss/all.scss';
+const data = [];
+
+// Generate 12 data entries
+for (let i = 0; i < 12; i++) {
+    const item = {
+        "id": i,
+        "name": "Poppy & Barley",
+        "imgUrl": `../assets/images/products${i + 1}.jpg`, 
+        "brand": "Jo Malone",
+        "salePrice": 1380,
+        "originalPrice": 1580
+    };
+    data.push(item);
+}
+
+const list = document.querySelector(".list");
+let str = '';
+
+data.forEach(function(item) {
+    str += `<li class="goods">
+                <div class="img-container">
+                    <img src="${item.imgUrl}" alt="${item.name}">
+                </div>
+                <div class="goods-content SF">
+                    <h3>${item.name}</h3>
+                    <a href="#">${item.brand}</a>
+                    <div class="mb4"><span class="saleoff mr8">NT$${item.salePrice}</span><span class="price"><del class="g-3">NT${item.originalPrice}</del></span></div>
+                    <div>
+                        <a href="#"><span class="material-symbols-sharp brown mr12">favorite</span></a>
+                        <a href="#"><span class="material-symbols-sharp brown">shopping_cart</span></a>
+                    </div>
+                </div>
+            </li>`;
+});
+
+list.innerHTML = str;
+
